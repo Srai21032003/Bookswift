3ec78dd962b60db14c82fca11106f70fe34f5fcc
sumit-2006 <sumitmahajan2006@gmail.com>
2024-09-28 17:51:47 +0530

    redirection

--- frontend/src/pages/SuccessPage.jsx
+++ frontend/src/pages/SuccessPage.jsx
@@ -1,10 +1,15 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 
 function SuccessPage() {
+    const navigate = useNavigate();
+    const handleLoginRedirect = () => {
+        navigate('/login'); // Redirect to the login page
+      };
     return (
         <div className="success-container">
             <h1>Registration Successful!</h1>
-            <p>Thank you for registering. You can now <a href="/login">Log in</a>.</p>
+            <p>Thank you for registering. You can now <a href="#" onClick={handleLoginRedirect}>Login</a>.</p>
         </div>
     );
 }
